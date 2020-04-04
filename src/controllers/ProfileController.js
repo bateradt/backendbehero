49903016d6ed2a1ef98f07a9c3bd62436b92b6a2
bateradt <marcelofscarpim@gmail.com>
2020-04-04 18:10:58 -0300

    Criado teste de requisições.

--- src/controllers/ProfileController.js
+++ src/controllers/ProfileController.js
@@ -5,7 +5,7 @@ module.exports = {
     const ong_id = request.headers.authorization;
     const incidents = await connection("incidents")
       .where("ong_id", ong_id)
-      .select("*");
+      .select(" * ");
 
       //console.log(incidents);
 
